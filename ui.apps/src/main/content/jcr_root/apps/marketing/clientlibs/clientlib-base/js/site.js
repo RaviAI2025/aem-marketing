d91098aa10681795a83362a02eabc87bf6271d1e
RaviAI2025 <ravirajendran2023@gmail.com>
2026-02-08 17:59:38 -0500

    Update site.js

--- ui.apps/src/main/content/jcr_root/apps/marketing/clientlibs/clientlib-base/js/site.js
+++ ui.apps/src/main/content/jcr_root/apps/marketing/clientlibs/clientlib-base/js/site.js
@@ -1,4 +1,5 @@
 /* Marketing Base JavaScript */
+/* This is my first commit to AEM and to validate changes are synching to Aggregator
 (function() {
     "use strict";
     console.log("Marketing clientlib loaded.");
